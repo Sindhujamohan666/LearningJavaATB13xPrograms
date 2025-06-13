658df4ca27f5a1dd4f48a52aaa7f2dfb5d66d688
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-13 14:45:54 +0530

    ex 2

---
+++ src/ex_02_Java_Basic/Lab016_Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic;
+
+public class Lab016_Variables {
+    public static void main(String[] args) {
+        int a=10;
+        a=65+1;
+        //output 66 var a is re-assigned
+        System.out.println(a);
+    }
+}
