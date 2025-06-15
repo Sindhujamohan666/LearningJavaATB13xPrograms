0637f3eb170a5d9c9200e84795b2a1b2d7f79ff4
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-15 07:51:43 +0530

    typecast1

---
+++ src/ex_09_If_Condition/Lab080_If.java
@@ -0,0 +1,14 @@
+package ex_09_If_Condition;
+
+import java.util.Scanner;
+
+public class Lab080_If {
+    public static void main(String[] args) {
+        System.out.println("Enter the age");
+        Scanner scanner=new Scanner(System.in);
+        int age=scanner.nextInt();
+        if(age>18){
+            System.out.println("You can vote");
+        }
+    }
+}
