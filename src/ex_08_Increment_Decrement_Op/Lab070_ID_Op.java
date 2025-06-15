a1f4778d7f42c3ae604a22f93663ea4ee5d560aa
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-15 07:35:33 +0530

    typecast1

---
+++ src/ex_08_Increment_Decrement_Op/Lab070_ID_Op.java
@@ -0,0 +1,16 @@
+package ex_08_Increment_Decrement_Op;
+
+public class Lab070_ID_Op {
+    public static void main(String[] args) {
+        int a=10;
+        int b=++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        //  Exp and Result Table
+        // Line No | a | Result b
+        //5 |10 | NA
+        //6 | 11(NA) | 11
+        //7 | 11 | 11(NA)
+    }
+}
