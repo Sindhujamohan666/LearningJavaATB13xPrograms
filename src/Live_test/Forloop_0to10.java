cce6d2c64a0afb2e154dcb50705b18b941a6c303
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-19 10:19:15 +0530

    leap year

---
+++ src/Live_test/Forloop_0to10.java
@@ -0,0 +1,9 @@
+package Live_test;
+
+public class Forloop_0to10 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Live_test/Switch_days.java
@@ -0,0 +1,20 @@
+package Live_test;
+import java.util.Scanner;
+public class Switch_days {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the days");
+        int day=s.nextInt();
+        switch(day){
+            case 1->System.out.println("Monday");
+            case 2->System.out.println("Tuesday");
+            case 3->System.out.println("Wednesday");
+            case 4->System.out.println("Thursday");
+            case 5->System.out.println("Friday");
+            case 6->System.out.println("Saturday");
+            case 7->System.out.println("Sunday");
+
+        }
+
+    }
+}
