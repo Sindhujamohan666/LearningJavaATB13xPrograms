3e9cb11d08efe574c052d35ca2813f5d50f2d256
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-22 20:45:41 +0530

    for loop

--- src/Live_test/Increment.java
+++ src/Live_test/Increment.java
@@ -2,17 +2,21 @@ package Live_test;
 
 public class Increment {
     public static void main(String[] args) {
-        int a=5;
+        int i=5;
         //output
-        System.out.println("Pre-increment:"+(++a));
-        System.out.println("Post-increment:"+(a++));
-        System.out.println("Post-decrement:"+(a--));
-        System.out.println("Pre-decrement:"+(--a));
+        int preinrement=++i;
+        System.out.println("pre-inrement "+preinrement);
+        int postincrement=i++;
+        System.out.println("post-inrement "+postincrement);
+        int predecrement=--i;
+        System.out.println("pre-decrement "+predecrement);
+        System.out.println("post-decrement "+(i--));
 
-        //6/exp 6/ a=6
-        //7/ exp 6  /7
-        //8/ exp 7/6
-        //9/exp 5  /5
+
+        //7/exp 6/ a=6
+        //8/ exp 6  /7
+        //8/ exp 6/6
+        //9/exp 5  /6
 
 
     }
