b7919a1fcfd40457868719c716d3383d685bae52
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-22 20:33:11 +0530

    for loop

--- src/Live_test/Forloop_0to10.java
+++ src/Live_test/Forloop_0to10.java
@@ -1,8 +1,13 @@
 package Live_test;
 
+import java.util.Scanner;
+
 public class Forloop_0to10 {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
             System.out.println(i);
         }
     }
