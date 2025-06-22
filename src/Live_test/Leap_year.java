9b93972476b8adb15cc0cfa9ebbe3e59517d51b9
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-22 20:22:29 +0530

    leap year

--- src/Live_test/Leap_year.java
+++ src/Live_test/Leap_year.java
@@ -2,11 +2,11 @@ package Live_test;
 
 public class Leap_year {
     public static void main(String[] args) {
-        int year=2000;
+        int year=2020;
         if((year%4==0)||(year%400==0)){
-            System.out.println("Leap year" +year);
+            System.out.println(year + " is a leap year");
         }
         else
-            System.out.println("Not a leap year" +year);
+            System.out.println(year + " is not a leap year");
     }
 }
