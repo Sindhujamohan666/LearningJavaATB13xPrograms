608d951de547068170faca2534dd934e885a9825
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-22 21:30:51 +0530

    leap year

--- src/Live_test/Leap_year.java
+++ src/Live_test/Leap_year.java
@@ -1,9 +1,13 @@
 package Live_test;
 
+import java.util.Scanner;
+
 public class Leap_year {
     public static void main(String[] args) {
-        int year=2020;
-        if((year%4==0)||(year%400==0)){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter any year");
+        int year=s.nextInt();
+        if((year%4==0 && year%100!=0)||(year%400==0)){
             System.out.println(year +  " is a leap year");
         }
         else
