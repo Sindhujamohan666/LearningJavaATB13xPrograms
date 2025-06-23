13618960880ed16813a0c34eccb1616f869d5148
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-23 22:15:03 +0530

    maxoftwo

---
+++ src/Tasks/Task_2306_Max_two.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task_2306_Max_two {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter number 1.....");
+        int num1=scanner.nextInt();
+        System.out.println("Enter number 2.....");
+        int num2=scanner.nextInt();
+        if(num1>num2){
+            System.out.println(num1+ " is greater");
+        }
+        else{
+            System.out.println(num2+ " is greater");
+        }
+    }
+}
