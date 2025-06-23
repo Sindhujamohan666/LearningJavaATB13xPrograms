66a24c48ba63cfd4b5905da3232514354a499aeb
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-23 22:28:05 +0530

    vowel

---
+++ src/Tasks/Task_2306_vowel_if_else.java
@@ -0,0 +1,16 @@
+package Tasks;
+import java.util.Scanner;
+public class Task_2306_vowel_if_else {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter any character.....");
+        char ch=scanner.next().toLowerCase().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            System.out.println(ch+ " is a vowel");
+        }
+        else
+        {
+            System.out.println(ch+ " is not a vowel");
+        }
+    }
+}
---
+++ src/Tasks/Task_2306_vowel_switch.java
@@ -0,0 +1,14 @@
+package Tasks;
+import java.util.Scanner;
+public class Task_2306_vowel_switch {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter any character.....");
+        char ch=scanner.next().toLowerCase().charAt(0);
+        switch(ch){
+            case 'a','e','i','o','u'->System.out.println(ch+ " is vowel");
+            default->System.out.println(ch+ " is not a vowel");
+        }
+
+    }
+}
