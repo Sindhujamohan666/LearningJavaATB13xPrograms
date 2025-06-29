873f54aa67576eab1b028705b542b50456259e2b
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-29 15:29:42 +0530

    27 june task

---
+++ src/Tasks/Convert_Days_to_Years_Months_Days.java
@@ -0,0 +1,18 @@
+package Tasks;
+import java.util.Scanner;
+public class Convert_Days_to_Years_Months_Days {
+    public static void main(String[] args) {
+        //if you input 1000 days, the output of the program would be:
+        // 2 years, 9 months, and 0 days.
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of days");
+        int input=sc.nextInt();
+        int years=input/365;
+        int remaining_days=input%365;
+        int months=remaining_days/30;
+        int days_count=remaining_days%30;
+        System.out.println(years +" years," + months +" months," +days_count +" days");
+
+    }
+
+}
