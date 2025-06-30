7c684a53b54f8cf3f8945b61f7b6c2d119db52db
Sindhujamohan666 <sindhupositive@gmail.com>
2025-06-30 07:58:55 +0530

    27 june task

--- src/Tasks2406/Prime_number.java
+++ src/Tasks2406/Prime_number.java
@@ -9,6 +9,8 @@ public class Prime_number {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         boolean isPrime=true;
+        /*skips doing i%2 for the initial i value(skips 2) if (i=3;i<number;i++)-skips
+        i%2 for 3*/
         for(int i=2;i<number;i++){
             if(i%2==0){
                 isPrime=false;
