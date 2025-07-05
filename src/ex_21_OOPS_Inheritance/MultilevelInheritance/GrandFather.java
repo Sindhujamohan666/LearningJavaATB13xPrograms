32036b25681f3ae1eebcc0a008f7cefa900fd714
Sindhujamohan666 <sindhupositive@gmail.com>
2025-07-05 15:19:47 +0530

    inheritance

---
+++ src/ex_21_OOPS_Inheritance/MultilevelInheritance/GrandFather.java
@@ -0,0 +1,11 @@
+package ex_21_OOPS_Inheritance.MultilevelInheritance;
+
+public class GrandFather {
+    void gf(){
+
+    }
+    int gold_gf = 10;
+    void home(){
+        System.out.println("GF-S1");
+    }
+}
