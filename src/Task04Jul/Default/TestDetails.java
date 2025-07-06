f00c5d294ba0c00738545997d6d336572ed8b04b
Sindhujamohan666 <sindhupositive@gmail.com>
2025-07-06 21:55:37 +0530

    default

---
+++ src/Task04Jul/Default/TestDetails.java
@@ -0,0 +1,11 @@
+package Task04Jul.Default;
+
+public class TestDetails {
+    public static void main(String[] args) {
+        //Student stud=new Student();
+       // stud.showDetails();
+       /* show details is default ..it cannot be accessed in diff package with only obj creation
+       Package has to b imported.
+        */
+    }
+}
